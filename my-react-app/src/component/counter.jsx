72d25f872d4ade97197e58031c2d709ea505eafe
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-09 17:07:38 +0200

    img added

--- my-react-app/src/component/counter.jsx
+++ my-react-app/src/component/counter.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 class Counter extends Component {
-    state = {  }
-    render() { 
+    state = {
+        count: 0,
+        urlImg:"https://datapp.fr/site/wp-content/uploads/2013/03/image-alignment-150x150.jpg",
+    };
+    style = {
+        fontSize: 20,
+        color: "red",
+    };
+    render() {
         return (
-            <h1>Hello world!</h1>
+            <>
+                <h1>Hello World!</h1>
+                <button>Ajouter</button>
+                <p>Valeur du compteur {this.state.count}</p>
+                <p>Valeur du compteur {this.formatCounter()}</p>
+                <img src={this.state.urlImg} alt="BelleImage" />
+            </>
         );
+    }
+    formatCounter() {
+        const { count } = this.state;
+        return count == 0 ? <small>Zero</small> : count;
     }
 }
 
