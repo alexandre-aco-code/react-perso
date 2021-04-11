d7584ec14fc3f3770b22095863c40e090bed7f11
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-11 15:20:33 +0200

    img src

--- my-react-app/src/component/counter.jsx
+++ my-react-app/src/component/counter.jsx
@@ -1,11 +1,13 @@
-import { Component } from 'react';
+import { Component } from "react";
+import logo192 from "../logo192.png";
 
 class Counter extends Component {
     state = {
         count: 0,
         counter: 2,
-        urlImg: "https://datapp.fr/site/wp-content/uploads/2013/03/image-alignment-150x150.jpg",
-        jours: ["lundi","mardi","mercredi"],
+        urlImg:
+            "https://datapp.fr/site/wp-content/uploads/2013/03/image-alignment-150x150.jpg",
+        jours: ["lundi", "mardi", "mercredi"],
     };
     style = {
         fontSize: 20,
@@ -15,55 +17,65 @@ class Counter extends Component {
         return (
             <>
                 <h1>Hello World!</h1>
-                <button className={this.getClassBtn()} onClick={this.onClickButton.bind(this)}>Ajouter</button>
+                <button
+                    className={this.getClassBtn()}
+                    onClick={() => this.onClickButton({id:1})}
+                >
+                    Ajouter
+                </button>
                 <p style={this.style}>Valeur du compteur {this.state.count}</p>
-                <p style={{
-                    color: "green",
-                    fontWeight: "normal"
-                }}>Valeur du compteur {this.formatCounter()}</p>
+                <p
+                    style={{
+                        color: "green",
+                        fontWeight: "normal",
+                    }}
+                >
+                    Valeur du compteur {this.formatCounter()}
+                </p>
                 <img src={this.state.urlImg} alt="BelleImage" />
-                <ul style={{
-                    // text-align: "left",
-                    color: "green",
-                    }}>
-                    {this.state.jours.map(jour =>
-                        (<li key={jour}> {jour} </li>)
-                    )}
+                <ul
+                    style={{
+                        // text-align: "left",
+                        color: "green",
+                    }}
+                >
+                    {this.state.jours.map((jour) => (
+                        <li key={jour}> {jour} </li>
+                    ))}
                 </ul>
 
-                <p>{this.afficherJours()}</p>
+                {this.afficherJours()}
+
+                <img src={logo192} alt=""/>
             </>
         );
     }
+
     formatCounter() {
         return this.state.count == 0 ? "zero" : this.state.count;
     }
 
     getClassBtn() {
-        const { counter } = this.state;
-        return counter == 0 ? "btn btn-danger" : "btn btn-success";
+        const { count } = this.state;
+        return count == 0 ? "btn btn-danger" : "btn btn-success";
     }
 
-    afficherJours() { 
-        if (this.state.jours.length == 0)
-            return <p>aucun jour de dispo</p>
+    afficherJours() {
+        if (this.state.jours.length == 0) return <p>aucun jour de dispo</p>;
         else
-            return <ul>
-                {this.state.jours.map(jour => 
-                    <li>{jour}</li>)}
-            </ul>
+            return (
+                <ul>
+                    {this.state.jours.map((jour) => (
+                        <li key={jour}>{jour}</li>
+                    ))}
+                </ul>
+            );
     }
 
-    // onClickButton() {
-    //     console.log(this);
-    // } 
-
-    onClickButton = () => {
-        this.setState({count : this.state.count + 1});
-    }
-
-
-    // this.setState(this.state.count, this.state.count++);
+    onClickButton = (element) => {
+        console.log(element)
+        this.setState({ count: this.state.count + 1 });
+    };
 
 }
 
