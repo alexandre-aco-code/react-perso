59ad6a678ce071e208e2f9fcd9f36c59a00d2872
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-11 15:07:39 +0200

    counter works

--- my-react-app/src/component/counter.jsx
+++ my-react-app/src/component/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
     state = {
         count: 0,
         counter: 2,
-        urlImg:"https://datapp.fr/site/wp-content/uploads/2013/03/image-alignment-150x150.jpg",
+        urlImg: "https://datapp.fr/site/wp-content/uploads/2013/03/image-alignment-150x150.jpg",
+        jours: ["lundi","mardi","mercredi"],
     };
     style = {
         fontSize: 20,
@@ -14,13 +15,23 @@ class Counter extends Component {
         return (
             <>
                 <h1>Hello World!</h1>
-                <button className={this.getClassBtn()}>Ajouter</button>
+                <button className={this.getClassBtn()} onClick={this.onClickButton.bind(this)}>Ajouter</button>
                 <p style={this.style}>Valeur du compteur {this.state.count}</p>
                 <p style={{
                     color: "green",
-                fontWeight:"normal"
+                    fontWeight: "normal"
                 }}>Valeur du compteur {this.formatCounter()}</p>
                 <img src={this.state.urlImg} alt="BelleImage" />
+                <ul style={{
+                    // text-align: "left",
+                    color: "green",
+                    }}>
+                    {this.state.jours.map(jour =>
+                        (<li key={jour}> {jour} </li>)
+                    )}
+                </ul>
+
+                <p>{this.afficherJours()}</p>
             </>
         );
     }
@@ -32,6 +43,28 @@ class Counter extends Component {
         const { counter } = this.state;
         return counter == 0 ? "btn btn-danger" : "btn btn-success";
     }
+
+    afficherJours() { 
+        if (this.state.jours.length == 0)
+            return <p>aucun jour de dispo</p>
+        else
+            return <ul>
+                {this.state.jours.map(jour => 
+                    <li>{jour}</li>)}
+            </ul>
+    }
+
+    // onClickButton() {
+    //     console.log(this);
+    // } 
+
+    onClickButton = () => {
+        this.setState({count : this.state.count + 1});
+    }
+
+
+    // this.setState(this.state.count, this.state.count++);
+
 }
 
 export default Counter;
