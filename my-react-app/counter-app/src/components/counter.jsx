19581d0bec747a5e492aeaa99ec8e3df8eb06d2b
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-12 14:39:25 +0200

    increment decrement counters fully works

--- my-react-app/counter-app/src/components/counter.jsx
+++ my-react-app/counter-app/src/components/counter.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: this.props.valeur
-  };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onAdd(this.props.compteur)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.compteur.id)}
+          className="btn btn-danger btn-sm"
+        >
+          Decrement
         </button>
       </div>
     );
@@ -25,13 +24,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.compteur.valeur === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const {valeur} = this.props.compteur;
+    return valeur === 0 ? "Zero" : valeur;
   }
 }
 
--- my-react-app/counter-app/src/components/counters.jsx
+++ my-react-app/counter-app/src/components/counters.jsx
@@ -7,14 +7,51 @@ class Counters extends Component {
             { id: 1, valeur:4},
             { id: 2, valeur:3},
             { id: 3, valeur:0},
-            { id: 4, valeur:18},
         ]
     }
+
+    delete = id => {
+        // console.log("delete", id)
+        const compteurClone = [...this.state.compteurs];
+        // console.log("compteurClone", compteurClone);
+        // console.log("compteurClone[id-1]", compteurClone[id-1]);
+
+
+        compteurClone[id - 1].valeur--;
+        // console.log("compteurClone[id-1]", compteurClone[id - 1]);
+
+        this.setState({compteurs: compteurClone})
+    }
+
+    add = objet => {
+        // console.log("add", objet);
+        const index = this.state.compteurs.indexOf(objet);
+
+        const compteurClone = [...this.state.compteurs];
+        
+        console.log(compteurClone[index]);
+
+        compteurClone[index] = { ...objet };
+        
+        compteurClone[index].valeur++;
+        
+        // console.log(compteurClone);
+        this.setState({ compteurs: compteurClone })
+    }
+        
+        
     render() { 
         return (
             <React.Fragment>
                 {this.state.compteurs.map(compteur => {
-                    return <Counter key={compteur.id} valeur={compteur.valeur}/>
+                    return (
+                        <Counter
+                        key={compteur.id}
+                        compteur={compteur}
+                        onDelete={this.delete}
+                        onAdd={this.add}
+                        />
+                    )
                 })}
             </React.Fragment>
         );
