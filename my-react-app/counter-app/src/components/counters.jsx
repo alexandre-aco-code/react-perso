49295ad3a55eadd25c46fdce80b34f7dc9b0bbc7
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-12 11:41:53 +0200

    props works

--- my-react-app/counter-app/src/components/counters.jsx
+++ my-react-app/counter-app/src/components/counters.jsx
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {  }
+    state = {
+        compteurs: [
+            { id: 1, valeur:4},
+            { id: 2, valeur:3},
+            { id: 3, valeur:0},
+            { id: 4, valeur:18},
+        ]
+    }
     render() { 
         return (
             <React.Fragment>
-                <Counter />
-                <Counter />
-                <Counter />
-                <Counter />
+                {this.state.compteurs.map(compteur => {
+                    return <Counter key={compteur.id} valeur={compteur.valeur}/>
+                })}
             </React.Fragment>
         );
     }
