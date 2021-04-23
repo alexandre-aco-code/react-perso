b8e65472d77e1fef2d331b3ad0c2474329d6a37b
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-23 20:04:02 +0200

    form works

--- formulaire/src/components/loginForm.jsx
+++ formulaire/src/components/loginForm.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-    state = {};
+    state = {
+        profil: {
+            login: "",
+            password: ""
+        }
+    };
+
+    connexion = e => {
+        e.preventDefault();
+        console.log("tentative de connexion" + " " + this.state.profil.login);
+    }
+
+    change = e => {
+        const profil = { ...this.state.profil };
+        //profil.login = e.currentTarget.value;
+        profil[e.currentTarget.name] = e.currentTarget.value;
+        this.setState({ profil: profil });
+    }
+
     render() {
+        const { profil } = this.state;
         return (
             <>
-                <form>
+                <form onSubmit={this.connexion}>
                     <div className="form-group">
                         <label htmlFor="login">Login</label>
                         <input
@@ -13,17 +32,24 @@ class LoginForm extends Component {
                             className="form-control"
                             id="login"
                             placeholder="login"
+                            value={profil.login}
+                            onChange={this.change}
+                            name="login"
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Login</label>
+                        <label htmlFor="password">Password</label>
                         <input
                             type="text"
                             className="form-control"
                             id="password"
                             placeholder="password"
+                            value={profil.password}
+                            onChange={this.change}
+                            name="password"
                         />
                     </div>
+                    <p>{this.state.profil.login}</p>
                     <button type="submit" className="btn btn-primary">
                         Connexion
                     </button>
