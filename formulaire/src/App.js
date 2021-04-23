72ac2564eae9217f39799362ca926852bf9330c1
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-23 11:38:23 +0200

    form created

---
+++ formulaire/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./components/loginForm.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <header className="row">
+        <div className="col-sm-6">
+          <h1 className="text-center">Titre principal</h1>
+        </div>
+      </header>
+      <section className="row">
+        <div className="col-sm-6 offset md-3">
+          {/* <LoginForm /> */}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
