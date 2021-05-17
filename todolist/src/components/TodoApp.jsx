ca659053f68f573628bcde49db993c5b64f84bff
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-05-17 13:39:41 +0200

    more clean code

--- todolist/src/components/TodoApp.jsx
+++ todolist/src/components/TodoApp.jsx
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TodoApp extends Component {
     constructor() {
         super();
-        this.state = { items: ["faire à manger", "faire la vaisselle"], textprovisoire:'' }
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target.value);
-        // console.log(e.target[0].value);
-
-        const newItems = [...this.state.items, e.target[0].value];
-
-        this.setState({ items: newItems })
-
+        this.state = {
+            items: ["faire à manger", "faire la vaisselle"],
+            textprovisoire: "",
+        };
     }
 
     handleChange = (e) => {
         e.preventDefault();
-        
-        this.setState({ textprovisoire: e.target.value })
-    }
+        this.setState({ textprovisoire: e.target.value });
+    };
 
-
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const newItems = [...this.state.items, this.state.textprovisoire];
+        this.setState({ items: newItems });
+    };
 
     render() {
-
-        const TodoList = this.state.items.map((task, index) => <li key={index}>{task}</li>);
+        const TodoList = this.state.items.map((task, index) => (
+            <li key={index}>{task}</li>
+        ));
 
         return (
             <div>
                 <h1>Todolist</h1>
-                <ul>
-                    {TodoList}
-                </ul>
+                <ul>{TodoList}</ul>
 
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="new-todo">Que faut t'il faire chef ?</label>
-                    <input id="new-todo" onChange={this.handleChange} value={this.state.textprovisoire}/>
+                    <input
+                        id="new-todo"
+                        value={this.state.textprovisoire}
+                        onChange={this.handleChange}
+                    />
                     <button>Ajouter une tâche gros</button>
                 </form>
-
-
             </div>
         );
     }
