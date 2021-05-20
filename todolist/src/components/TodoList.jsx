c01a7d4574950275119ffbdfc694473a017aa1b4
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-05-20 14:01:45 +0200

    morpion game created, click on case works

--- todolist/src/components/TodoList.jsx
+++ todolist/src/components/TodoList.jsx
@@ -2,9 +2,18 @@ import { Component } from "react";
 
 class TodoList extends Component {
     state = {};
+
+    deleteTask = (e) => {
+        console.log(e.target);
+        this.setState();
+    };
+
     render() {
         const TodoList = this.props.items.map((task, index) => (
-            <li key={index}>{task}</li>
+            <li key={index}>
+                {task}
+                <button onClick={this.deleteTask}>x</button>
+            </li>
         ));
 
         return (
