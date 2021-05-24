970ad05dc60f962b6a2f740f991f995f8b03ad9f
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-05-24 13:49:19 +0200

    code good but api not working Access-Control-Allow-Origin error

--- dota/src/components/Heros.jsx
+++ dota/src/components/Heros.jsx
@@ -8,14 +8,32 @@ export default function Heros() {
     // Remarque : le tableau vide de dépendances [] indique
     // que useEffect ne s’exécutera qu’une fois, un peu comme
     // componentDidMount()
+
+    // const Dota2Api = require("dota2-api");
+
+    // const da = Dota2Api.create("F1C3BD20842DC2863C697D6D48DE6600", 570);
+
+    // const options = { game_mode: 1 };
+    // da.getMatchHistory(options).then(
+    //     (result) => {
+    //         console.log(result);
+    //     },
+    //     (errorResponseStatusText) => {
+    //         console.log(errorResponseStatusText);
+    //     }
+    // );
+
+
     useEffect(() => {
-        fetch("https://api.stratz.com/api/v1/Hero/1/purchasePattern")
+        fetch(
+            "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=00525C0E3751C36DEDC4102061BE66E3"
+        )
             .then((res) => res.json())
             .then(
                 (result) => {
-                    console.log(result)
+                    console.log(result);
                     setIsLoaded(true);
-                    // setItems(result);
+                    setItems(result);
                 },
                 // Remarque : il faut gérer les erreurs ici plutôt que dans
                 // un bloc catch() afin que nous n’avalions pas les exceptions
